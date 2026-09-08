c8207acfb1361edc2c0544a948f14704fdbf75d2
Lucky-dev3755 <rlucky3755@gmail.com>
2026-09-08 01:00:52 -0700

    add score  system
    Updated the number of attempts variable and improved output messages.

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import random
 
 number = random.randint(1, 100)
+max_attempts = 7
 
 print("Number Guessing Game")
 print("1 se 100 ke beech number guess karo!")
 print("Aapko 7 chances milenge.")
 
-for attempt in range(1, 8):
+for attempt in range(1, max_attempts + 1):
     guess = int(input("Apna guess dalo: "))
 
     if guess < number:
         print("Thoda bada number try karo!")
+
     elif guess > number:
         print("Thoda chhota number try karo!")
+
     else:
+        score = max_attempts - attempt + 1
         print("Sahi jawab!")
-        print("Aap", attempt, "attempts mein jeet gaye!")
+        print("Aapne", attempt, "attempts mein jeet gaye!")
+        print("Aapka score:", score)
         break
+
 else:
     print("Game Over!")
     print("Sahi number tha:", number)
