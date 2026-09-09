b6086ff4ca3a3033494dd2a1913799d2e1e40211
Lucky-dev3755 <rlucky3755@gmail.com>
2026-09-09 02:49:49 -0700

    game lost statistics

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ except (FileNotFoundError, PermissionError, ValueError):
     best_score = 0
     total_game = 0
     games_won = 0
+    games_lost = 0
 while True:
     print("\nNumber Guessing Game")
     print("Current Best Score:", best_score)
@@ -107,13 +108,14 @@ else:
             break
 
     else:
+        games_lost += 1
         print("Game Over!")
         print("Sahi number tha:", number)
 
     print("Best score:", best_score)
     print("Total Games:", total_games)
 print("Games Won:", games_won)
-
+print("Games Lost:", games_lost)
 win_rate = (games_won / total_games) * 100
 print("Win Rate:", round(win_rate, 1), "%")
     again = input("Dobara khelna hai? (y/n): ")
